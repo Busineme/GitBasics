d4a98077af4d286b2faf20408aace69ac23fb272
Lucas <lucassalves65@gmail.com>
2015-04-21 13:35:53 -0300

    this commit closes aldarionsevero/Busineme/GitBasics#1
    Signed-off-by: Lucas <lucassalves65@gmail.com>

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+#aloalo
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'biblioteca.views.home', name='home'),
